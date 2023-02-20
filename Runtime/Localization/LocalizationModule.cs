4035c9cd0462812c0e92d870aebee4672cf3d44a
Mirzipan <mfarkas@pixelfederation.com>
2023-02-20 22:23:40 +0100

    🚧stub for localization module

---
+++ Runtime/Localization/LocalizationModule.cs
@@ -0,0 +1,16 @@
+﻿using Mirzipan.Framed.Modules;
+
+namespace Mirzipan.Framed.Localization
+{
+    public class LocalizationModule: CoreModule
+    {
+        #region Lifecycle
+
+        protected override void OnInit()
+        {
+            // TODO
+        }
+
+        #endregion Lifecycle
+    }
+}

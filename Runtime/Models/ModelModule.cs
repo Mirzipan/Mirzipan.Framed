272c3b155a3102e70c4ee97203229fdab1ab6b28
Mirzipan <mfarkas@pixelfederation.com>
2023-02-20 22:23:49 +0100

    🚧stub for model module

---
+++ Runtime/Models/ModelModule.cs
@@ -0,0 +1,16 @@
+﻿using Mirzipan.Framed.Modules;
+
+namespace Mirzipan.Framed.Models
+{
+    public class ModelModule: CoreModule
+    {
+        #region Lifecycle
+
+        protected override void OnInit()
+        {
+            // TODO
+        }
+
+        #endregion Lifecycle
+    }
+}

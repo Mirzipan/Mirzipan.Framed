7aef84660889a217f72d966ee877e05957f0eb32
Mirzipan <mfarkas@pixelfederation.com>
2023-04-30 21:30:51 +0200

    ✨added AddSingletonAsSelf extension

---
+++ Runtime/Extensions/ContainerDescriptorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Reflex.Core;
+
+namespace Mirzipan.Framed.Extensions
+{
+    public static class ContainerDescriptorExtensions
+    {
+        public static ContainerDescriptor AddSingletonAsSelf(this ContainerDescriptor @this, Type type, params Type[] contracts)
+        {
+            Array.Resize(ref contracts, contracts.Length + 1);
+            contracts[^1] = type;
+            @this.AddSingleton(type, contracts);
+            return @this;
+        }
+    }
+}

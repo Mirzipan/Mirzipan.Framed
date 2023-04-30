53717ccbc78e15dac40316cc1f7a76102c724766
Mirzipan <mfarkas@pixelfederation.com>
2023-04-30 21:44:50 +0200

    ✨added another Inject extension for component

--- Runtime/Extensions/ComponentExtensions.cs
+++ Runtime/Extensions/ComponentExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using Reflex.Core;
 using Reflex.Enums;
+using Reflex.Injectors;
 using UnityEngine;
 
 namespace Mirzipan.Framed.Extensions
@@ -17,6 +19,14 @@ namespace Mirzipan.Framed.Extensions
                 MonoInjectionMode.Recursive => component.GetComponentsInChildren<MonoBehaviour>(true),
                 _ => throw new ArgumentOutOfRangeException(nameof(injectionMode), injectionMode, null)
             };
+        }
+        
+        public static void Inject(this Component @this, Container container, MonoInjectionMode injectionMode = MonoInjectionMode.Recursive)
+        {
+            foreach (var injectable in @this.GetInjectables(injectionMode))
+            {
+                AttributeInjector.Inject(injectable, container);
+            }
         }
     }
 }

e31e8f9cfa60d9b7daf3af99c38b0f22da8913ba
Mirzipan <mfarkas@pixelfederation.com>
2023-05-01 18:48:54 +0200

    ✨added extensions for binding singleton as interface or interfaces and self

--- Runtime/Extensions/ContainerDescriptorExtensions.cs
+++ Runtime/Extensions/ContainerDescriptorExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Mirzipan.Extensions.Collections;
 using Reflex.Core;
 
 namespace Mirzipan.Framed.Extensions
@@ -14,8 +15,31 @@ namespace Mirzipan.Framed.Extensions
             
             Array.Resize(ref contracts, contracts.Length + 1);
             contracts[^1] = type;
-            @this.AddSingleton(type, contracts);
-            return @this;
+            return @this.AddSingleton(type, contracts);
+        }
+
+        public static ContainerDescriptor AddSingletonAsInterfacesAndSelf(this ContainerDescriptor @this, Type type)
+        {
+            var interfaces = type.GetInterfaces();
+            if (interfaces.IsNullOrEmpty())
+            {
+                return @this.AddSingleton(type);
+            }
+            
+            Array.Resize(ref interfaces, interfaces.Length + 1);
+            interfaces[^1] = type;
+            return @this.AddSingleton(type, interfaces);
+        }
+
+        public static ContainerDescriptor AddSingletonAsInterfaces(this ContainerDescriptor @this, Type type)
+        {
+            var interfaces = type.GetInterfaces();
+            if (interfaces.IsNullOrEmpty())
+            {
+                return @this.AddSingleton(type);
+            }
+            
+            return @this.AddSingleton(type, interfaces);
         }
     }
 }
